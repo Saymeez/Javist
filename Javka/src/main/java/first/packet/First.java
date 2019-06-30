17056cd8e325ef2215288dbb0ea1cde1624c0614
Saymeez <52138103+Saymeez@users.noreply.github.com>
2019-06-30 17:33:19 +0300

    IDK

--- Javka/src/main/java/first/packet/First.java
+++ Javka/src/main/java/first/packet/First.java
@@ -1,11 +1,24 @@
 package first.packet;
 
+import java.sql.SQLOutput;
+
 public class First {
 
 	public static void main(String[] args) {
 
-System.out.println("Hello, world!!!");
-System.out.print("Hello, world!");
+		System.out.println("Hello, world!!!");
+
+		System.out.println(2 + 2);
+			System.out.println(2 * 2);
+				System.out.println(2 / 2);
+					System.out.println(2 - 2);
+
+
+					System.out.println(1.0 / 2);
+						System.out.println("2" + "2");
+
+
+
 
 }
 
